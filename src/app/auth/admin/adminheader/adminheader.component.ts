7d6c078c46f467be87889de2ec367b7bccd0f8b5
mujeeb <mujeebrahmanps01707@gmail.com>
2023-08-29 12:47:38 +0530

    admin logout clear jwt

--- src/app/auth/admin/adminheader/adminheader.component.ts
+++ src/app/auth/admin/adminheader/adminheader.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adminheader',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AdminheaderComponent {
 
-  onLogout(){}
+  constructor(private router:Router){}
+
+  onLogout(){
+    localStorage.removeItem('adminToken')
+    localStorage.removeItem('adminId')
+    this.router.navigate(['/auth/admin'])
+
+  }
 }
